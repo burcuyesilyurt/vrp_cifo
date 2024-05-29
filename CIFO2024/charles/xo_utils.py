0847cb10e197e82157bca249c67b41777423fa5b
carlos-rodrigues75 <carlos.rodrigues.dev@gmail.com>
2024-05-29 16:13:42 +0100

    Add more utility functions to the xo_utils file

--- CIFO2024/charles/xo_utils.py
+++ CIFO2024/charles/xo_utils.py
@@ -1,3 +1,6 @@
+from copy import copy
+
+
 def flatten_routes(individual):
     """
     Flat the individual representation from 2D-array to 1D-array,
@@ -21,14 +24,11 @@ def flatten_routes(individual):
 def reconstruct_routes(flat_offspring):
     offspring = []
 
-    current_route = None
+    current_route = []
     for i in flat_offspring:
         if i == 0:
-            if current_route is None:
-                current_route = []
-            else:
-                offspring.append(current_route)
-                current_route = None
+            offspring.append(current_route)
+            current_route = []
         else:
             current_route.append(i)
 
@@ -54,17 +54,52 @@ def repair_pickup(offspring, data):
 
     return offspring
 
+
 def repair_pickup_randomly(offspring, data):
     pass
 
+
 def fill_missing_pickups(offspring, data):
-    pass
+    pickups_by_id = {data[i][0]: i for i in offspring if data[i][1] == 'cp'}
+    all_pickups_by_id = {data[i][0]: i for i in range(len(data)) if data[i][1] == 'cp'}
+
+    result = copy(offspring)
+    for idx, i in enumerate(offspring):
+        if data[i][1] == 'cd' and data[i][8] not in pickups_by_id.keys():
+            result.insert(idx, all_pickups_by_id[data[i][8]])
+
+    return result
+
+
+def fill_missing_deliveries(offspring, data):
+    deliveries_by_id = {data[i][0]: i for i in offspring if data[i][1] == 'cd'}
+    all_deliveries_by_id = {data[i][0]: i for i in range(len(data)) if data[i][1] == 'cd'}
+
+    result = copy(offspring)
+    for i in offspring:
+        if data[i][1] == 'cp' and data[i][8] not in deliveries_by_id.keys():
+            result.insert(offspring.index(i)+1, all_deliveries_by_id[data[i][8]])
+
+    return result
 
 def fill_missing_pickups_randomly(offspring, data):
     pass
 
-def fill_missing_deliveries(offspring, data):
-    pass
 
 def fill_missing_deliveries_randomly(offspring, data):
     pass
+
+
+def remove_duplicates(offspring):
+    """
+    Remove duplicates of the offspring, only keeping the first occurence
+    of each element. Zeroes are not considered and all zeroes are kept.
+    """
+    visited_elements = set()
+    result = []
+    for i in offspring:
+        if i not in visited_elements or i == 0:
+            result.append(i)
+            visited_elements.add(i)
+
+    return result
