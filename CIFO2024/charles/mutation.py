4c5835bd61a288926d1a86083764edcfee061f2a
dcavaleiro <devoracavaleiro27@hotmail.com>
2024-05-30 13:01:08 +0100

    swap_mutation working in case of empty list

--- CIFO2024/charles/mutation.py
+++ CIFO2024/charles/mutation.py
@@ -12,10 +12,27 @@ def swap_mutation(individual):
     """
     mut_list_index = randrange(len(individual))
     chosen_list = individual[mut_list_index]
+
+    # Ensure chosen_list has more than 1 element for a meaningful swap
+    while len(chosen_list) <= 1:
+        mut_list_index = randrange(len(individual))
+        chosen_list = individual[mut_list_index]
+
+    # Perform the swap mutation on the chosen list
     mut_indexes = sample(range(len(chosen_list)), 2)
     chosen_list[mut_indexes[0]], chosen_list[mut_indexes[1]] = chosen_list[mut_indexes[1]], chosen_list[mut_indexes[0]]
+
     return individual
 
+
+    """
+    mut_list_index = randrange(len(individual))
+    chosen_list = individual[mut_list_index]
+    if len(chosen_list) == 0:
+        mut_indexes = sample(range(len(chosen_list)), 2)
+        chosen_list[mut_indexes[0]], chosen_list[mut_indexes[1]] = chosen_list[mut_indexes[1]], chosen_list[mut_indexes[0]]
+    return individual
+"""
 
 def inversion_mutation(individual):
     """Inversion mutation for a GA individual. Reverts a portion of the representation.
@@ -33,5 +50,5 @@ def inversion_mutation(individual):
 
 
 if __name__ == "__main__":
-    test = [[0,1,9,7,5,4], [3,7], [4,6,7], [4,2,6]]
+    test = [[0,1,9,7,5,4], [], [4,6,7], [4,2,6]]
     swap_mutation(test)
