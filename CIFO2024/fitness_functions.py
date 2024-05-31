a774c0d579c770aa262128e8e9b547acf4b19a0b
David <davidguarin9@gmail.com>
2024-05-31 13:11:22 +0100

    fitness battery

--- CIFO2024/fitness_functions.py
+++ CIFO2024/fitness_functions.py
@@ -2,7 +2,7 @@ from utils import euclidean_distance
 from restrictions import has_pickup_violation, has_capacity_violation
 
 
-def get_fitness(data):
+def get_fitness(data, charge):
     def get_overall_fitness(individual):
         #TODO implement the fitness function considering the different objectives
         # We can try weighted sum or pareto front here
@@ -19,7 +19,7 @@ def get_fitness(data):
             #if has_pickup_violation(route, data):
              #   return 500000000
 
-            fitness_time += get_fitness_time(route, data)
+            fitness_time += get_fitness_time(route, data, charge)
             fitness_capacity += get_fitness_capacity(route,data)
             
                 
@@ -44,7 +44,7 @@ def get_fitness_capacity(route, data):
         return 1
     
 
-def get_fitness_time(route, data):
+def get_fitness_time(route, data,charge):
     # Assign d0 to be equals the depot
     d0 = data[0]
     delay = 0
@@ -60,14 +60,51 @@ def get_fitness_time(route, data):
         service_time = float(data[rep][7])
 
         if delivery_first != 100000:
+
             distance = euclidean_distance(data[rep], data[delivery_first])
             battery -= distance
-            current_time += distance
+            
 
+            if rep == len(data)-1:
+                next_distance = euclidean_distance(data[rep], d0)
+
+                if (battery-next_distance) < 0:
+                    min_distance = 10000
+                    
+                    for i in range(0, len(charge)):
+
+                        charge_station = charge[i]
+
+                        d = euclidean_distance(data[rep], charge_station)
+                        d=+ euclidean_distance(d0, charge_station)
+
+                        if d < min_distance:
+                            min_distance = d
+                            
+                    distance += min_distance
+                    battery = 77.75
+                else:
+                    next_distance = euclidean_distance(data[rep], data[rep+1])
+                    if (battery-next_distance) < 0:
+                        min_distance = 10000
+                        station_name = ''
+                        for i in range(0, len(charge)):
+                            charge_station = charge[i]
+                            d = euclidean_distance(data[rep], charge_station)
+                            d=+ euclidean_distance(data[rep+1], charge_station)
+                            if d < min_distance:
+                                min_distance = d
+                                station_name = charge_station[0]
+                        distance += min_distance
+                        battery = 77.75
+            current_time += distance
         else:
+
             distance = euclidean_distance(data[rep], d0)
             battery -= distance
             current_time += distance
+
+
 
         if ready_time > current_time:
             current_time = ready_time
@@ -94,7 +131,10 @@ def get_fitness_time(route, data):
      #   time_error = True
         
    
-
+    print("--------------------")
+    print(current_time, delay)
+    print(current_time*0.3, delay*0.7)
+    print("---------------------")
     return (current_time*0.3 + delay*0.7)
 
 
